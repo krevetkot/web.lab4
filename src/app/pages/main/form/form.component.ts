fae36b6fea164f55cccc1f03ac1e1aa20ffd7c99
krevetkot <ksenia.slonimskaya@yandex.ru>
2024-12-03 00:12:42 +0300

    start doing mainForm

--- src/app/pages/main/form/form.component.ts
+++ src/app/pages/main/form/form.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { SliderModule } from 'primeng/slider';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators, NgModel, FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [
+    SliderModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
+export class FormComponent implements OnInit{
+  mainForm!: FormGroup;
+  yValue: any;
+  constructor(private router: Router,
+              private authService: AuthService) {}
 
+  submitMainForm(){}
+
+  ngOnInit(): void {
+    this.mainForm = new FormGroup({
+      'yValue': new FormControl('', [
+        Validators.min(-3), Validators.max(5)]),
+      'password': new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+      ])
+    })
+  }
 }
