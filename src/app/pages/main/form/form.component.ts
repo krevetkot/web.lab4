670d67c85a5c67020ce8b5831d196c48528fa356
krevetkot <ksenia.slonimskaya@yandex.ru>
2024-12-03 22:28:11 +0300

    pretty form

--- src/app/pages/main/form/form.component.ts
+++ src/app/pages/main/form/form.component.ts
@@ -1,9 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, importProvidersFrom, OnInit} from '@angular/core';
 import { SliderModule } from 'primeng/slider';
 import { KnobModule} from 'primeng/knob';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators, NgModel, FormsModule} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AuthService} from '../../../services/auth.service';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-form',
@@ -11,26 +13,34 @@ import {AuthService} from '../../../services/auth.service';
     SliderModule,
     KnobModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    CardModule,
+    ButtonModule
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent implements OnInit{
-  // mainForm: FormGroup;
+  mainForm!: FormGroup;
   yValue: number = 0;
   xValue: number = 0;
+  rValue: number = 1;
+  loading: boolean = false;
   constructor(private router: Router,
               private authService: AuthService) {}
 
-  submitMainForm(){}
+  submitMainForm(){
+    this.loading = true;
+    console.log(this.mainForm.value);
+    this.loading = false;
+  }
 
   ngOnInit(): void {
-    // this.mainForm = new FormGroup({
-    //   'xValue': new FormControl('', [
-    //     Validators.min(-3), Validators.max(5)]),
-    //   'yValue': new FormControl('', [
-    //     Validators.min(-10), Validators.max(10)])
-    // })
+    this.mainForm = new FormGroup({
+      'xValue': new FormControl('', [
+        Validators.min(-3), Validators.max(5)]),
+      'yValue': new FormControl('', [
+        Validators.min(-10), Validators.max(10)])
+    })
   }
 }
