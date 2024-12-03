cfbdbbfba3e1d9cd1778b32a2d3454fe589a7eb3
krevetkot <ksenia.slonimskaya@yandex.ru>
2024-12-03 20:37:14 +0300

    start doing shitty form

--- src/app/pages/main/form/form.component.ts
+++ src/app/pages/main/form/form.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { SliderModule } from 'primeng/slider';
+import { KnobModule} from 'primeng/knob';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators, NgModel, FormsModule} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AuthService} from '../../../services/auth.service';
@@ -8,6 +9,7 @@ import {AuthService} from '../../../services/auth.service';
   selector: 'app-form',
   imports: [
     SliderModule,
+    KnobModule,
     ReactiveFormsModule,
     FormsModule
   ],
@@ -15,21 +17,20 @@ import {AuthService} from '../../../services/auth.service';
   styleUrl: './form.component.scss'
 })
 export class FormComponent implements OnInit{
-  mainForm!: FormGroup;
-  yValue: any;
+  // mainForm: FormGroup;
+  yValue: number = 0;
+  xValue: number = 0;
   constructor(private router: Router,
               private authService: AuthService) {}
 
   submitMainForm(){}
 
   ngOnInit(): void {
-    this.mainForm = new FormGroup({
-      'yValue': new FormControl('', [
-        Validators.min(-3), Validators.max(5)]),
-      'password': new FormControl('', [
-        Validators.required,
-        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
-      ])
-    })
+    // this.mainForm = new FormGroup({
+    //   'xValue': new FormControl('', [
+    //     Validators.min(-3), Validators.max(5)]),
+    //   'yValue': new FormControl('', [
+    //     Validators.min(-10), Validators.max(10)])
+    // })
   }
 }
