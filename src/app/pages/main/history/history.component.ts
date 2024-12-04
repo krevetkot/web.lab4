88bddaa6f151d7b4c5686d01129579e0e5a88d5c
krevetkot <ksenia.slonimskaya@yandex.ru>
2024-12-04 15:41:07 +0300

    table

--- src/app/pages/main/history/history.component.ts
+++ src/app/pages/main/history/history.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { ScrollerModule } from 'primeng/scroller';
+import {CardModule} from 'primeng/card';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  imports: [
+    TableModule,
+    ScrollerModule,
+    CardModule
+  ],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit{
+  // points: Point[] = [];
+  points = [
+    { x: 1, y: 1, r: 1 }
+  ];
 
+  addPoint(x: number, y: number, r: number): void {
+    this.points.push({ x, y, r });
+  }
+
+  // Получение всех точек
+  getAllPoints(): Point[] {
+    return Object.values(this.points);
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
+
+
+export interface Point {
+  x: number;
+  y: number;
+  r: number;
 }
